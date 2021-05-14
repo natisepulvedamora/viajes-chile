022ff9dad1d98c0c51d371daa82dd7f182aefeb4
nesmo <natisepulvedamora@gmail.com>
2021-05-13 23:32:07 -0400

    comentando código

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,10 +20,11 @@ $(function(){
     // Cambiando el color del navbar al hacer scroll
     if ( $(window).scrollTop() > $('header').height()) {
       // Si el scroll es mayor al tamaño del header dejará de ser transparente
+      //y cambiará el color de la letra también
         $('#nav-color').css("background-color",'#f5f6f5');
         $('#nav-color a').css("color",'#383534');
     } else {
-      // Si está en el header vuelve a ser transparente
+      // Si está en el header vuelve a los valores iniciales
         $('#nav-color').css("background-color",'transparent');
         $('#nav-color a').css("color",'rgb(222, 221, 216)');
         
