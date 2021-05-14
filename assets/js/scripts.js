39c6a764d7cd37e41a3392d34380c83d4b8ca137
nesmo <natisepulvedamora@gmail.com>
2021-05-13 23:28:34 -0400

    agregando query

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,37 @@
 $(function(){
 
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-      })
-      
-    
+  // Código para que funcione el formulario importado de typeform
+  //  Código de bootstrap para que funcione el tooltip
+   $(function () {
+       $('[data-toggle="tooltip"]').tooltip();
+     });
+     // Función para ocultar y mostrar el texto de las tarjetas al hacer click en el título de las tarjetas
+   $('.card-title').click(function(){
+       let dis=$('.card-text').css("display");
+       if(dis==='block'){
+           $('.card-text').hide();
+       }
+       else{
+           $('.card-text').show();
+       }
+   });
 
+   $(document).on('scroll', function(){
+    // Cambiando el color del navbar al hacer scroll
+    if ( $(window).scrollTop() > $('header').height()) {
+      // Si el scroll es mayor al tamaño del header dejará de ser transparente
+        $('#nav-color').css("background-color",'#f5f6f5');
+        $('#nav-color a').css("color",'#383534');
+    } else {
+      // Si está en el header vuelve a ser transparente
+        $('#nav-color').css("background-color",'transparent');
+        $('#nav-color a').css("color",'rgb(222, 221, 216)');
+        
+    }
+});
+
+
+  
      
 
 });
